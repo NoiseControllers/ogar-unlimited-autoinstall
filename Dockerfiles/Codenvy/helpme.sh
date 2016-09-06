27bb4c5565a1182fe0d2ea56831e3832d3f29905
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2016-09-06 08:02:30 +0000

    Update helpme.sh

--- Dockerfiles/Codenvy/helpme.sh
+++ Dockerfiles/Codenvy/helpme.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-apt-get install ruby -y
+sudo apt-get install ruby -y
 cd /opt
-wget https://github.com/busyloop/lolcat/archive/master.zip
-unzip master.zip
-rm -Rf master.zip
+sudo wget https://github.com/busyloop/lolcat/archive/master.zip
+sudo unzip master.zip
+sudo rm -Rf master.zip
 cd lolcat-master/
-gem install lolcat
+sudo gem install lolcat
+sudo npm cache clean -f | lolcat -a -s 100
+sudo npm install -g n | lolcat -a -s 100
+sudo n 5.9.0 | lolcat -a -s 100
+sudo ln -sf /usr/local/n/versions/node/5.9.0/bin/node /usr/bin/node | lolcat -a -s 100
+sudo npm config set prefix /usr/local | lolcat -a -s 100
 clear
 sleep 0.3
 cd ~/
-mkdir ~/ogar/
+sudo mkdir ~/ogar/
 cd ~/ogar/
-git clone https://github.com/AJS-development/Ogar-unlimited.git
+sudo git clone https://github.com/AJS-development/Ogar-unlimited.git
 cd Ogar-unlimited/src/
 clear
 sleep 0.3
 npm install | lolcat -a -s 100
-clear
+
